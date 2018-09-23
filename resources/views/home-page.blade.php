f4bbbc762d4a61a3bf92c20baa890ea238729200
Rehan Mazhar <rehansyed89@gmail.com>
2018-09-23 12:59:34 +0200

    Part 11 adding dynamic menus (except for the footer menu)

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -9,20 +9,8 @@
                 <div class="logo">Ecommerce</div>
             </div>
             <div class="top-nav-right">
-                <ul>
-                    <li><a href="{{ route('shop.index') }}">Shop</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li>
-                        <a href="{{ route('cart.index') }}">Cart
-                            <span class="cart-count">
-                            @if(Cart::instance('default')->count() > 0)
-                                    <span>{{Cart::instance('default')->count()}}</span>
-                                @endif
-                        </span>
-                        </a>
-                    </li>
-                </ul>
+                <!--Main menu -->
+                {{ menu('main', 'partials.menus.main') }}
             </div>
         </div> <!--end top-nav-->
 
---
+++ resources/views/partials/menus/main.blade.php
@@ -0,0 +1,15 @@
+<ul>
+    @foreach($items as $menu_item)
+        <li><a href="{{ $menu_item->link() }}">
+                <span>{{ $menu_item->title }}</span>
+                @if($menu_item->title === 'Cart')
+                    @if(Cart::instance('default')->count() > 0)
+                        <span class="cart-count">
+                            <span>{{Cart::instance('default')->count()}}</span>
+                        </span>
+                    @endif
+                @endif
+            </a>
+        </li>
+    @endforeach
+</ul>
